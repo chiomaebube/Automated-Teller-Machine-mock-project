30927756f9617402360ef40d50453dd3867e4a14
Chioma Oko <chiomaebubeoko@gmail.com>
2021-04-23 14:43:06 +0100

    update

--- Automated Teller Machine mock project.py
+++ Automated Teller Machine mock project.py
@@ -3,111 +3,135 @@ import random
 
 database = {}
 
+
 def init():
-
-    correctOptionisselected = False
     print('You are welcome to Mybank plc')
+    accVerification = int(input('Do you have an account with us? Type 1(Yes) or 2(No)\n'))
+    if (accVerification == 1):
 
-    while correctOptionisselected == False:
+        login()
 
-        accVerification = input('Do you have an account with us? Type 1(Yes) or 2(No)\n')
-        if(accVerification == 1):
-            correctOptionisselected = True
-            login()
+    elif (accVerification == 2):
 
-        elif(accVerification == 2):
-            correctOptionisselected = True
-            print(register())
+        print(register())
 
-        else:
-            print("Please try again; enter 1 or 2 next time") 
+    else:
+        print("Please try again; enter 1 or 2 next time")
+        init()
+
 
 def login():
-    print('Please enter your details to login')
+    print('Please enter your details to login\n')
+    print("===========================================\n")
 
-    loginSuccessful = False
+    username = input("What is your username?\n")
+    password = input("Enter your password:\n")
 
-    while loginSuccessful == False:
-   
-        username = input("What is your username?\n")
-        password = input("Enter your password:\n")
-           
-        for userDetails in database.items():
-            if (userDetails[1] == username):
-                if(password[3] == password):
+    for accNumber, userDetails in database.items():
 
-                    print("What's up %s" % username)
-                    loginSuccessful = True
+        if (userDetails[2] == username):
+            if (userDetails[4] == password):
 
-                    dT = datetime.datetime.now()
+                print("What's up %s" % userDetails[0])
 
-                    print ("Today's date:  = %s/%s/%s" % (dT.day, dT.month, dT.year))
-                    print ("The time is now: = %s:%s" % (dT.hour, dT.minute))
-                   
+                dT = datetime.datetime.now()
 
-                else:
-                    print("Password incorrect, please try again")
+                print("Today's date:  = %s/%s/%s" % (dT.day, dT.month, dT.year))
+                print("The time is now: = %s:%s" % (dT.hour, dT.minute))
+
+                bankOperation(userDetails)
+
+
 
             else:
-                print("Name not found, try again")
-    bankOperation(userDetails)
+                print("Password incorrect, please try again")
+                login()
+
+        else:
+            print("Name not found, try again")
+            login()
+
 
 def register():
     print('Please enter your details to login\n\n')
 
-    Fullname = input('Enter your full name, First name first\n')
-    username = input('Enter your preferred username\n')
-    email = input('Enter your email\n')
-    password = input('Enter your password\n')
+    firstName = input("Enter your First name, \n")
+    lastName = input("Enter your Last name, \n")
+    username = input("Enter your preferred username\n")
+    email = input("Enter your email\n")
+    password = input("Enter your password\n")
 
-    accNumber = GenAcctNum()
+    accNumber = genAcctNum()
 
-    database[accNumber] = [ Fullname, username, email, password ]
+    database[accNumber] = [firstName, lastName, username, email, password]
     print("Your account has been created")
-    print("Here's your account number" + accNumber)
+    print("Here's your account number %s\n" % accNumber)
 
-   # return database
+    # return database
 
     login()
-   
+
+
 def bankOperation(user):
-    currentbalance = 0.00
-    print("Welcome %s, what will you like to do today?" % user[0])
-    print("1. Withdrawal" )
+    print("What will you like to do today?\n")
+    print("1. Withdrawal")
     print("2. Cash Deposit")
     print("3. Complaint")
+    print("4. Logout")
 
     selectedOption = int(input("Please selcect an option i.e 1:\n"))
 
     if (selectedOption == 1):
         print("You want to Withdraw!")
-        amountToWithdraw = int(input(" How much would you like to withdraw\n"))
-        balanceRemaining = currentbalance - amountToWithdraw
-        print("Recieved")
-        print("Take your cash" + balanceRemaining)
+        withdrawal()
+
 
     elif (selectedOption == 2):
         print("You want to Deposit Cash!")
-        amountToDeposit = float(input("How much would you like to deposit?\n"))
-        currentbalance += amountToDeposit
-        print("Your account ballance is now ",currentbalance)
-
+        deposit()
 
     elif (selectedOption == 3):
         print("You want to Complain!")
-        issue = input("What issue will you like to report?\n") 
-        print("TComplaint recieved!")
-        print("Thank you for contacting us")
+        complain()
 
-
+    elif (selectedOption == 4):
+        logout()
     else:
+
         print("Invalid option selected, please try again")
+        bankOperation(user)
 
-def GenAcctNum():
 
+def genAcctNum():
     print("Account number loading...\n\n")
     return random.randrange(0000000000, 9999999999)
-  
+
+
+def withdrawal():
+    currentbalance = 0.00
+    amountToWithdraw = int(input(" How much would you like to withdraw\n"))
+    balanceRemaining = currentbalance - amountToWithdraw
+    balanceRemaining == currentbalance
+    print("Recieved")
+    print("Take your cash %s" % balanceRemaining)
+
+
+def deposit():
+    currentbalance = 0.00
+    amountToDeposit = float(input("How much would you like to deposit?\n"))
+    currentbalance + amountToDeposit
+    print("Your account balance is now %s " % currentbalance)
+
+
+def complain():
+    issue = input("What issue will you like to report?\n")
+    print("Complaint received!")
+    print("Thank you for contacting us")
+
+
+def logout():
+    print("You want to Logout!")
+
 
 #### BANKING SYSTEM STARTS ####
 
